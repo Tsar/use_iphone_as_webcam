41373ed78c1a38f59669884628d3cb7b7090a84d
Ioann Volkov <volkov.ioann@gmail.com>
2026-05-24 17:37:45 +0300

    Read RTSP transport from RTSP_TRANSPORT env var, default to tcp

--- play_stream_from_iphone.sh
+++ play_stream_from_iphone.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 IPHONE_IP="${IPHONE_IP:-172.20.10.1}"
+RTSP_TRANSPORT="${RTSP_TRANSPORT:-tcp}"
 
-ffplay -rtsp_transport udp -fflags nobuffer -flags low_delay -framedrop -strict experimental rtsp://admin:admin@${IPHONE_IP}:8554/live
+ffplay -rtsp_transport "${RTSP_TRANSPORT}" -fflags nobuffer -flags low_delay -framedrop -strict experimental rtsp://admin:admin@${IPHONE_IP}:8554/live
 #
 ## or sometimes faster:
 #
-#ffplay -rtsp_transport udp -fflags nobuffer -flags low_delay -probesize 32 -analyzeduration 0 -sync video rtsp://admin:admin@${IPHONE_IP}:8554/live
+#ffplay -rtsp_transport "${RTSP_TRANSPORT}" -fflags nobuffer -flags low_delay -probesize 32 -analyzeduration 0 -sync video rtsp://admin:admin@${IPHONE_IP}:8554/live
